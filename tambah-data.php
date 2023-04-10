c82846e923987f0953caab765145c722ff7ad706
Muhta Nuryadi <nuryadi.muhta@gmail.com>
2023-04-10 22:12:00 +0700

    apa aja

--- tambah-data.php
+++ tambah-data.php
@@ -1,6 +1,26 @@
 <?php
 $title = "Form Tambah Data";
+
 include "layout/header.php";
 
 
 ?>
+
+<div class="card container mt-2">
+    <div class="card-body">
+        <h5 class="card-title">Form Tambah Data</h5>
+        <p class="card-text">Kelas B1-B2</p>
+    </div>
+</div>
+
+<div class="container mt-3">
+    <form action="" method="post" enctype="multipart/form-data">
+
+        <div class="container mt-3">
+            <label for="nim" class="form-label">NIM</label>
+            <input type="text" class="form-control" id="nim" name="nim" placeholder="Masukkan NIM anda">
+        </div>
+
+    </form>
+
+</div>
