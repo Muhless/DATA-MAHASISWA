b46fe92bf554a1d179f47e5b8b025652231fd9ba
Muhta Nuryadi <nuryadi.muhta@gmail.com>
2023-04-12 15:53:32 +0700

    fungsi tambah data mahasiswa

--- config/controller.php
+++ config/controller.php
@@ -1,5 +1,6 @@
 <?php
-function select($query){
+function select($query)
+{
     global $db;
 
     $result = mysqli_query($db, $query);
@@ -11,3 +12,71 @@ function select($query){
     return $rows;
 }
 
+
+// fungsi tambah data
+function tambah_data($post)
+{
+    global $db;
+
+    $nim            = strip_tags($post['nim']);
+    $nama           = strip_tags($post['nama']);
+    $jenis_kelamin  = strip_tags($post['jenis_kelamin']);
+    $foto           = upload_file();
+    $alamat         = strip_tags($post['alamat']);
+
+
+    // check upload foto
+    if (!$foto) {
+        return false;
+    }
+
+    $query = "INSERT INTO kelas_b VALUES (null, '$nim', '$nama', '$jenis_kelamin', '$foto', '$alamat');";
+
+    mysqli_query($db, $query);
+
+    return mysqli_affected_rows($db);
+}
+
+
+// fungsi upload file
+function upload_file()
+{
+    global $db;
+
+    $namaFile           = $_FILES['foto']['name'];
+    $ukuranFile         = $_FILES['foto']['size'];
+    $error              = $_FILES['foto']['error'];
+    $tmpname            = $_FILES['foto']['tmp_name'];
+
+    // check file yang duipload
+    $ekstensifileValid   = ['jpg', 'jpeg', 'png'];
+    $ekstensifile        = explode('.', $namaFile);
+    $ekstensifile        = strtolower(end($ekstensifile));
+
+    // check format ekstensi file
+    if (!in_array($ekstensifile, $ekstensifileValid)) {
+        echo "<script>
+            alert('Format File Tidak Valid');
+            document.location.href = 'tambah-data.php';
+            </script>";
+        die();
+    }
+
+    // check ukuran file <2MB
+    if ($ukuranFile > 2048000) {
+        echo "<script>
+                alert('File Melebihi Batas Maximum 2MB');
+                document.location.href = 'tambah-data.php';
+                </script>";
+        die();
+    }
+
+    // generate nama file baru
+    $namaFileBaru   = uniqid();
+    $namaFileBaru   .= '.';
+    $namaFileBaru   .= $ekstensifile;
+
+    // pindahkan hasil upload ke folder
+    move_uploaded_file($tmpname, 'assets/img/' . $namaFileBaru);
+    return $namaFileBaru;
+}
--- tambah-data.php
+++ tambah-data.php
@@ -4,6 +4,20 @@ $title = "Form Tambah Data";
 include "layout/header.php";
 
 
+if(isset($_POST['tambah'])){
+    if (tambah_data($_POST) > 0 ){
+        echo "<script>
+            alert('Data berhasil ditambahkan');
+            document.location.href = 'index.php';
+            </script>";
+    } else {
+        echo "<script>
+            alert('Data gagal ditambahkan');
+            document.location.href = 'index.php';
+            </script>";
+    }
+}
+
 ?>
 
 <div class="card container mt-2">
@@ -13,14 +27,47 @@ include "layout/header.php";
     </div>
 </div>
 
-<div class="container mt-3">
+<div class="container mt-2">
     <form action="" method="post" enctype="multipart/form-data">
 
-        <div class="container mt-3">
+        <div class="container mt-2">
             <label for="nim" class="form-label">NIM</label>
-            <input type="text" class="form-control" id="nim" name="nim" placeholder="Masukkan NIM anda">
+            <input type="number" class="form-control" id="nim" name="nim" placeholder="Masukkan NIM anda..." required>
         </div>
 
+        <div class="container mt-2">
+            <label for="nama" class="form-label">Nama</label>
+            <input type="nama" class="form-control" id="nama" name="nama" placeholder="Masukkan Nama anda..." required>
+        </div>
+
+        <div class="container mt-2">
+            <label for="jenis_kelamin" class="form-label">Jenis Kelamin</label>
+            <select name="jenis_kelamin" class="form-select" id="jenis_kelamin" required>
+                <option value="">Pilih Jenis Kelamin...</option>
+                <option value="1">Laki-Laki</option>
+                <option value="2">Perempuan</option>
+            </select>
+        </div>
+
+        <div class="container mt-2">
+            <label for="foto" class="form-label">Foto</label>
+            <input type="file" class="form-control" id="foto" name="foto" required>
+        </div>
+
+        <div class="container mt-2">
+            <label for="alamat" class="form-label">Alamat</label>
+            <input type="text" class="form-control" id="alamat" name="alamat" placeholder="Masukkan Alamat Anda..." required>
+        </div>
+
+
+        <div class="container mt-4">
+            <a href="index.php" class="btn btn-danger" ><i class="fa-solid fa-delete-left"> Kembali</i></a>
+            <button class="btn btn-primary" name="tambah" ><i class="fa-solid fa-plus"> Tambah</i></button>
+        </div>
     </form>
 
 </div>
+
+<?php
+include "layout/footer.php"
+?>
