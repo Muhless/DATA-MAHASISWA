03d73ae7647ecd78b41eec7ede2625b0e2a15d86
Muhta Nuryadi <nuryadi.muhta@gmail.com>
2023-04-14 15:18:01 +0700

    progress

--- layout/footer.php
+++ layout/footer.php
@@ -1,4 +1,18 @@
 <script src="https://kit.fontawesome.com/9be1b4beee.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+
+<!-- datatables -->
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/.13.4/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+
+<script>
+    $(document).ready(function () {
+    $('#table').DataTable();
+});
+</script>
+
+
 </body>
 
 </html>
